5194c1fb768a524f2c809fbcb060b4014a954838
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-23 22:57:04 +0530

    Added: Ability to create Linked List

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -4,6 +4,7 @@ public class LinkedListMain {
 
     public static void main(String[] args) {
 
+        System.out.println("Welcome to Data Structure Problems using Java Generics");
         LinkedList linkedList = new LinkedList();
 
         linkedList.push(70);
