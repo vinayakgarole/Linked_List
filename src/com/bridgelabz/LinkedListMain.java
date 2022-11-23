626b8587949a14579729340a2a321e10969f3b62
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-23 22:45:39 +0530

    Added: Ability to create Linked

---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class LinkedListMain {
+
+    public static void main(String[] args) {
+
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+
+        linkedList.print();
+    }
+}
--- src/com/bridgelabz/Node.java
+++ src/com/bridgelabz/Node.java
@@ -2,12 +2,11 @@ package com.bridgelabz;
 
 public class Node {
 
-    public int data;
-    public Node next;
+    int data;
+    Node next;
 
     public Node(int data) {
 
         this.data = data;
-        this.next = null;
     }
 }
